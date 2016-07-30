22fb3e434284bfe6c73b9d28aa8fb229019e41f8
nayrslohcin <nayr.slohcin@gmail.com>
2016-07-30 17:17:44 -0500

    updated yum file to remove scl, and add java

--- jenkins_custom.sh
+++ jenkins_custom.sh
@@ -2,10 +2,12 @@
 
 #gpgcheck=0
 #vi /etc/yum.repos.d/jenkins.repo
+rm /etc/yum.repos.d/CentOS-SCL.repo
 
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 sed -i 's/gpgcheck=1/gpgcheck=0/' /etc/yum.repos.d/jenkins.repo
 
+yum -y --disablerepo="epel" install java
 yum -y --disablerepo="epel" install jenkins
 service jenkins start
 chkconfig jenkins on
